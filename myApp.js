5cb2ed513251cf480747cd86e951d1d6712bb011
Mattias Lundström <mattias.lundstrom1214@gmail.com>
2021-04-21 22:24:32 +0200

    public folder

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ var app = express();
 console.log("Hello Express");
 
 app.get('/', function(req, res) {
-    absolutePath = __dirname + "views/index.html";
+    absolutePath = __dirname + "/public/index.html";
     res.sendFile(absolutePath);
 })
 
