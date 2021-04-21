494941b2079a9c6e5b44e76687293af586688dd2
Mattias Lundström <mattias.lundstrom1214@gmail.com>
2021-04-21 22:18:22 +0200

    added node intro tests

---
+++ myApp.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+
+
+console.log("Hello World");
+
+app.get('/', function(req, res) {
+    res.send('Response String');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
